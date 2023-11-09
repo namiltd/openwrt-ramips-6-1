22a4e666de37451886ffbbcc575198321350ae04
namiltd <namiltd@users.noreply.github.com>
2023-11-09 18:14:49 +0100

    ramips: add support for 6.1 kernel for mt7620

--- target/linux/ramips/files/drivers/net/ethernet/ralink/mtk_eth_soc.c
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/mtk_eth_soc.c
@@ -1365,6 +1365,7 @@ static int __init fe_init(struct net_device *dev)
 	struct fe_priv *priv = netdev_priv(dev);
 	struct device_node *port;
 	int err;
+	u8 mac_addr[ETH_ALEN];
 
 	if (priv->soc->reset_fe)
 		priv->soc->reset_fe(priv);
@@ -1384,13 +1385,15 @@ static int __init fe_init(struct net_device *dev)
 
 	fe_reset_phy(priv);
 
-	of_get_mac_address(priv->dev->of_node, dev->dev_addr);
+	of_get_mac_address(priv->dev->of_node, mac_addr);
 
 	/* If the mac address is invalid, use random mac address  */
-	if (!is_valid_ether_addr(dev->dev_addr)) {
+	if (!is_valid_ether_addr(mac_addr)) {
 		eth_hw_addr_random(dev);
 		dev_err(priv->dev, "generated random MAC address %pM\n",
 			dev->dev_addr);
+	} else {
+		dev_addr_set(dev, mac_addr);
 	}
 
 	err = fe_mdio_init(priv);
@@ -1655,7 +1658,11 @@ static int fe_probe(struct platform_device *pdev)
 		priv->tx_ring.tx_ring_size *= 4;
 		priv->rx_ring.rx_ring_size *= 4;
 	}
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+	netif_napi_add_weight(netdev, &priv->rx_napi, fe_poll, napi_weight);
+#else
 	netif_napi_add(netdev, &priv->rx_napi, fe_poll, napi_weight);
+#endif
 	fe_set_ethtool_ops(netdev);
 
 	err = register_netdev(netdev);
